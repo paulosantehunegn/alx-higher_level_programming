55ec311f395038d24f8da9edb2c74935607ed181
root <paulosantu79@gmail.com>
2022-10-11 12:35:09 -0700

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
-
-
-class Square:
-    """An empty Square class"""
-    pass
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            c += 1
+        except IndexError:
+            break
+    print()
+    return c
