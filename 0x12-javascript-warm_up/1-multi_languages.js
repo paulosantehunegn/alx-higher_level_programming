b5e740ac3ee203ad2ae87d33116e576a65158521
paulosantehunegn <106868050+paulosantehunegn@users.noreply.github.com>
2022-12-20 11:45:28 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
